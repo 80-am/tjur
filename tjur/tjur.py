907e821640ef7bda7924a471c0d7096ef97fd593
80-am <adam@flonko.com>
2020-01-21 20:02:02 +0100

    adds win per trade target and minor logging refactoring

--- tjur/tjur.py
+++ tjur/tjur.py
@@ -13,7 +13,8 @@ from strategies.macd import Macd
 
 tjur_path = sys.argv[0]
 tjur_dir = os.path.dirname(tjur_path)
-logging.basicConfig(filename=tjur_dir + '/log/tjur-' + str(datetime.timestamp(datetime.utcnow())) + '.log', format='%(asctime)s.%(msecs)06d %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+logging.basicConfig(filename=tjur_dir + '/log/tjur-' + str(datetime.timestamp(datetime.utcnow())) +
+    '.log', format='%(asctime)s.%(msecs)06d %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
 logging.Formatter.converter = time.gmtime
 
 API_KEY = config.BINANCE['api_key']
@@ -25,13 +26,18 @@ class Tjur:
     with open(tjur_dir +'/assets/start-up.txt', 'r') as f:
         print(f.read())
     ready = False
-    print('Start time(UTC): ' + str(datetime.utcnow()))
     binance = Binance(API_KEY, API_SECRET)
     symbol = input("Select symbol to pair: ").upper()
+    check_symbol = str(binance.cur_avg_price(symbol))
+    if ('Invalid symbol' in check_symbol):
+        print('Invalid symbol')
+        print('Exiting')
+        sys.exit()
+
     print("Available strategies:")
     print("[1] Moving Average Cross Over")
     print("[2] Moving Average Convergence/Divergence (MACD)")
-    strategy = int(input("Select strategy: "))
+    strategy = int(input("Select strategy: ") or 1)
 
     if (strategy == 1):
         custom = input("Use custom parameters? [y/N] ").lower()
@@ -53,17 +59,14 @@ class Tjur:
         time_frame = input('Enter time frame: ').lower()
         short_term = int(input('Select short-term period: '))
         long_term = int(input('Select long-term period: '))
+        win_target = float(input('Profit target per trade (%): '))
     else:
         average = 'sma'
-        time_frame = '5m'
-        short_term = 12
+        time_frame = '1h'
+        short_term = 9
         long_term = 26
+        win_target = 1.036
 
-    check_symbol = str(binance.cur_avg_price(symbol))
-    if ('Invalid symbol' in check_symbol):
-        print('Invalid symbol')
-        print('Exiting')
-        sys.exit()
     if (short_term <= 0 or long_term <= 0):
         print(short_term)
         print('Invalid period')
@@ -83,28 +86,25 @@ class Tjur:
         print('Exiting')
         sys.exit()
 
-    print('Trading ' + symbol)
+    print('Start time(UTC):', datetime.utcnow())
+    print('Trading', symbol)
+    logging.info('Starting trading ' + symbol)
     while not ready:
         if (strategy.is_ready()):
             ready = True
+            logging.info('Strategy ready')
 
-    def calculate_signals(strategy, symbol, time_frame):
+    def calculate_signals(strategy, symbol, time_frame, win_target):
         """
-        Calculates when to send signal to buy or sell
-
-        Returns:
-        1 (int): Signal to buy
-        0 (int): Signal to hold
-       -1 (int): Signal to sell
+        Calculates when to send signal to buy or sell.
         """
 
         signal = 0
         if (strategy.calculate_buy_signal() and signal == 0):
             buy_order = float(binance.get_latest_price(symbol)['price'])
-            take_profit = buy_order * 1.16
+            take_profit = buy_order * win_target
             signal = 1
-            print('Buying at: ' + str(buy_order))
-            print(datetime.utcnow())
+            print(datetime.utcnow(), 'Buying at:', str(buy_order))
             logging.info('Buying at: ' + str(buy_order))
 
             while (signal == 1):
@@ -112,25 +112,21 @@ class Tjur:
                 stop_loss = buy_order * 0.84
                 sell_signal = strategy.calculate_sell_signal()
 
-                if (sell_signal and stop_loss > latest_price
-                        or sell_signal and latest_price > buy_order * 1.08
-                        or latest_price > take_profit):
+                if ((sell_signal and stop_loss > latest_price)
+                    or (sell_signal and latest_price > take_profit)):
                     sell_order = latest_price
-                    print('Sold at: ' + str(sell_order))
-                    print(datetime.utcnow())
+                    print(datetime.utcnow(), 'Sold at:', str(sell_order))
                     logging.info('Sold at: ' + str(sell_order))
                     pl = Performance.calculate_pl(buy_order, sell_order)
-                    print('Margin: ' + str(round(pl, 3)) + '%')
-                    logging.info('Margin: ' + str(round(pl, 3)) + '%')
-
+                    print(datetime.utcnow(), 'Margin:', str(round(pl, 3)) + '%')
+                    logging.info('Margin:' + str(round(pl, 3)) + '%')
                     signal = 0
 
     try:
         while True:
-            calculate_signals(strategy, symbol, time_frame)
+            calculate_signals(strategy, symbol, time_frame, win_target)
 
     except KeyboardInterrupt:
-        print(binance.get_latest_price(symbol)['price'])
         print('Exiting')
         logging.warning('Exiting')
         sys.exit()
