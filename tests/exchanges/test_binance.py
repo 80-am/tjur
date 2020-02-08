a4a7106c304e5c1cf053f782c7ecd972110d34a3
80-am <adam@flonko.com>
2020-02-09 00:05:12 +0100

    adds CI Travis and unittests

---
+++ tests/exchanges/test_binance.py
@@ -0,0 +1,26 @@
+import time
+import unittest
+
+from tjur.exchanges.binance import Binance
+
+binance = Binance('', '')
+
+
+class BinanceTest(unittest.TestCase):
+
+    def test_connection(self):
+        conn = binance.check_connection()
+        self.assertEqual(str(conn['serverTime'])[:-4], str(
+            int(time.time()))[:-1])
+
+    def test_recent_trades(self):
+        trades = binance.get_recent_trades('LINKETH', 100)
+        self.assertTrue(len(trades) == 100)
+
+    def test_get_current_average_price(self):
+        price = binance.get_cur_avg_price('LINKETH')
+        self.assertGreater(price, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
